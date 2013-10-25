a7b74335632a529b726a1490a5bbb9ec5dc0d85c
GoodDayToDie <been_out_cruising-codeplex@yahoo.com>
2013-10-25 04:38:02 -0700

    Native Access Webserver version 0.4.8 Alpha Added support for binary and long (64-bit) reg values. Updated NativeAccess libraries. Better handling of errors and empty fields in the registry.

--- WebAccess/WebApplication.cs
+++ WebAccess/WebApplication.cs
@@ -2,7 +2,7 @@
  * WebAccess\WebApplication.cs
  * Author: GoodDayToDie on XDA-Developers forum
  * License: Microsoft Public License (MS-PL)
- * Version: 0.4.6
+ * Version: 0.4.8
  * Source: https://wp8webserver.codeplex.com
  *
  * Handles GET requests from the web server.
@@ -26,6 +26,7 @@ using HttpServer;
 using FileSystem;
 using Registry;
 using FileInfo = FileSystem.FileInfo;
+using nfs = FileSystem.NativeFileSystem;
 
 namespace WebAccess
 {
@@ -150,7 +151,6 @@ namespace WebAccess
 
 		private static String serviceFilesystem (HttpRequest req, Socket sock)
 		{
-			NativeFileSystem nfs = new NativeFileSystem();
 			if (req.UrlParameters.ContainsKey("path"))
 			{
 				String path = req.UrlParameters["path"];
@@ -296,35 +296,137 @@ namespace WebAccess
 					build.AppendLine("<table border=\"1\"><tr><th>Values</th><th>Type</th><th>Size</th><th>Data</th></tr>");
 					foreach (ValueInfo info in values)
 					{
-						build.Append("<tr><td>").Append(info.Name).Append("</td><td>").Append(info.Type.ToString())
+						String name = String.IsNullOrEmpty(info.Name) ? "<i>default</i>" : info.Name;
+						build.Append("<tr><td>").Append(name).Append("</td><td>").Append(info.Type.ToString())
 							.Append("</td><td>").Append(info.Length).AppendLine("</td><td>");
-						if (RegistryType.String == info.Type || RegistryType.VariableString == info.Type)
+						switch (info.Type)
 						{
-							String data;
-							if (NativeRegistry.ReadString(hk, path, info.Name, out data) && !String.IsNullOrEmpty(data))
-							{
-								build.Append(WebUtility.HtmlEncode(data));
-							}
-						}
-						else if (RegistryType.Integer == info.Type)
-						{
-							uint data;
-							if (NativeRegistry.ReadDWORD(hk, path, info.Name, out data))
-							{
-								build.Append(data);
-							}
-						}
-						else if (RegistryType.MultiString == info.Type)
-						{
-							String[] data;
-							if (NativeRegistry.ReadMultiString(hk, path, info.Name, out data)
-								&& (data != null) && (data.Length > 0))
-							{
-								build.Append(WebUtility.HtmlEncode(data[0]));
-								for (int i = 1; i < data.Length; i++)
+						case RegistryType.String:
+						case RegistryType.VariableString:
+							{	// Handle REG_SZ and REG_EXPAND_SZ
+								String data;
+								if (NativeRegistry.ReadString(hk, path, info.Name, out data) && !String.IsNullOrEmpty(data))
 								{
-									build.AppendLine("<br />").Append(WebUtility.HtmlEncode(data[i]));
+									build.Append(WebUtility.HtmlEncode(data));
 								}
+								break;
+							}
+						case RegistryType.Integer:
+							{	// Handle REG_DWORD
+								uint data;
+								if (NativeRegistry.ReadDWORD(hk, path, info.Name, out data))
+								{
+									build.Append(data).AppendFormat(" (0x{0:X8})", data);
+								}
+								break;
+							}
+						case RegistryType.Long:
+							{	// Handle REG_QWORD
+								ulong data;
+								if (NativeRegistry.ReadQWORD(hk, path, info.Name, out data))
+								{
+									// TODO: Display the DATETIME value in readable format
+									build.Append(data);
+								}
+								break;
+							}
+						case RegistryType.MultiString:
+							{	// Handle REG_MULTI_SZ
+								String[] data;
+								if (NativeRegistry.ReadMultiString(hk, path, info.Name, out data)
+									&& (data != null) && (data.Length > 0))
+								{
+									build.Append(WebUtility.HtmlEncode(data[0]));
+									for (int i = 1; i < data.Length; i++)
+									{
+										build.AppendLine("<br />").Append(WebUtility.HtmlEncode(data[i]));
+									}
+								}
+								break;
+							}
+						default:
+							{	// Handle all other data types as if REG_BINARY
+								if ((RegistryType.Binary == info.Type) && (4 == info.Length))
+								{
+									// Treat it as a DWORD
+									uint data;
+									if (NativeRegistry.ReadDWORD(hk, path, info.Name, out data))
+									{
+										build.Append(data).AppendFormat(" (0x{0:X8})", data);
+									}
+								}
+								else if ((RegistryType.Binary == info.Type) && (8 == info.Length))
+								{
+									// Treat it as a QWORD
+									ulong data;
+									if (NativeRegistry.ReadQWORD(hk, path, info.Name, out data))
+									{
+										// TODO: Display the DATETIME value in readable format
+										build.Append(data);
+									}
+								}
+								else
+								{
+									// Display as a binary hex sequence
+									byte[] data;
+									if (NativeRegistry.ReadBinary(hk, path, info.Name, out data))
+									{
+										if (null != data)
+										{
+											// Use a table layout
+											int linestart = 0;
+											build.Append("<pre>");
+											for (int i = 0; i < data.Length; i++)
+											{
+												build.AppendFormat("{0:X2} ", data[i]);
+												if (15 == (i & 15))
+												{
+													build.Append("| ");
+													// After every 16th value, print the text line
+													for (; linestart <= i; linestart++)
+													{
+														if ((data[linestart] >= 32) && (data[linestart] < 127))
+														{
+															build.Append((char)(data[linestart]));
+														}
+														else
+														{
+															build.Append("&#xB7;");
+														}
+													}
+													build.AppendLine();
+												}
+											}
+											// The last line may not have been a full 16 bytes
+											int emptybytes = 16 - (data.Length - linestart);
+											if (emptybytes < 16)
+											{
+												build.Append(' ', 3 * emptybytes).Append("| ");
+												for (; linestart < (data.Length); linestart++)
+												{
+													if ((data[linestart] >= 32) && (data[linestart] < 127))
+													{
+														build.Append((char)(data[linestart]));
+													}
+													else
+													{
+														build.Append("&#xB7;");
+													}
+												}
+											}
+											build.AppendLine("</pre>");
+										}
+										else
+										{	// No data!
+											build.Append("<i>NULL</i>");
+										}
+									}
+									else
+									{	// Error reading data
+										build.AppendFormat("<i>Error reading data: {0} ({0:X})</i>", NativeRegistry.GetError());
+									}
+								}
+								break;
 							}
 						}
 						build.AppendLine("</td></tr>");
